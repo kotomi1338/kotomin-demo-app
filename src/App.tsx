630220c67c1649d18fb93764f0fb707bc1f658a5
kotomi1338 <kotomin1216@gmail.com>
2021-12-28 15:30:29 +0900

    fix: タスクがないときの表示を修正

--- src/App.tsx
+++ src/App.tsx
@@ -34,13 +34,17 @@ const App = () => {
     <Container>
       <PageTitle>申告な問題が発生しました</PageTitle>
       <AddTask tasks={tasks} getTasksData={getTasksData}/>
-      <SubTitle>サボった人からのコメント</SubTitle>
-      <Hr/>
-      {tasks.sort((a, b) => b.id - a.id).map(task => {
-        return (
-          <Task task={task}/>
-        );
-      })}
+      {tasks.length > 0 && (
+        <>
+          <SubTitle>サボった人からのコメント</SubTitle>
+          <Hr/>
+          {tasks.sort((a, b) => b.id - a.id).map(task => {
+            return (
+              <Task task={task} key={task.id}/>
+            );
+          })}
+        </>
+      )}
     </Container>
   );
 }
